fc614f06be4223009403ab80b28eba032c27aa6f
Stan Chvila <schvila@gmail.com>
2020-12-29 15:49:15 +0100

    Net5 added

---
+++ DotNet5/ConsoleAwaitNet5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAwaitNet5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
